ed9d802eac1edcbba020932ac15ed02fca245bdd
omar <omargrinat9@gmail.com>
2022-02-15 20:42:35 +0100

    add validation to confirmation forms

--- src/components/DeleteConfirmationForm.jsx
+++ src/components/DeleteConfirmationForm.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom'
 
 function DeleteConfirmationForm() {
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const [buttonDisabled, setButtonDisabled] = useState(false);
     let navigate = useNavigate();
 
     return (
@@ -12,6 +13,7 @@ function DeleteConfirmationForm() {
             justify-content-center align-items-center
             flex-column min-vh-100 container"
             onSubmit={handleSubmit(data => {
+                setButtonDisabled(true);
                 const options = {
                     method: "DELETE",
                     body: JSON.stringify({ confirmationToken: data.code }),
@@ -28,14 +30,17 @@ function DeleteConfirmationForm() {
                         alert("Suppression réussie")
                         navigate("/")
                     }
-                })
+                }).finally(() => setButtonDisabled(false))
             })} >
             <h1 className='text-primary fw-bold mb-3 text-center'>Suppression d'annonce</h1>
             <h2 className='text-center'>Entrez le code qu'on vous a envoyé par email</h2>
-            <input className='form-control my-3 border-2' style={{ width: "50%" }} {...register("code")} type="text" />
-
-            <button className='btn btn-primary btn-lg px-5 text-white'>Valider</button>
+            <input className='form-control my-3 border-2' style={{ width: "60%" }} {...register("code", { required: true })} />
+            {errors.code && (<div className="px-5 alert-danger alert">veuillez entrez le code</div>)}
             <button
+                disabled={buttonDisabled}
+                className='btn btn-primary btn-lg px-5 text-white'>Valider</button>
+            <button
+                disabled={buttonDisabled}
                 type='button'
                 onClick={() => navigate("/")}
                 className='btn btn-secondary mt-3 btn-lg px-5 text-white'>Retour</button>
